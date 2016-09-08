4f361c9b9f12a2c91e015021ea8bc3079936a2c9
Jon Burns <jonburns10@gmail.com>
2016-09-08 14:30:00 -0700

    File migration

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Stylesheets -->
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,500,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/foundation/6.2.1/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="/colorado-mental-health-professionals/main.css">
+	<!-- Stylesheets -->
+
+	<title>2016 Colorado Springs Recovery Rally</title>
+</head>
+<body>
+<div class="img-title" style="background:url('http://res.cloudinary.com/dbc2wlvk8/image/upload/b_rgb:fff,c_fill,g_center,h_350,o_56,w_1500/v1464564119/shutterstock_133120892_mbwpkf.jpg') no-repeat fixed;">
+	<div class="head-text">
+		<img src="http://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,w_250/v1466377807/Springs-Recovery-Rally_waqkuq.png">	
+	</div>
+</div>
+
+<div class="row">
+	<div class="small-12 columns">
+		<h1>2016 Colorado Springs Recovery Rally</h1>
+		<h3>Event Date: 9/10/2016</h3>
+	</div>
+</div>
+<div class="large-tabs">
+	<div class="row hide-for-small-only">
+		<ul class="tabs" data-tabs id="event-tabs">
+		  <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Event Overview</a></li>
+		  <li class="tabs-title"><a href="#panel2">Program</a></li>
+		  <li class="tabs-title"><a href="#panel4">Exhibit</a></li>
+		  <li class="tabs-title"><a href="#panel5">Sponsors</a></li>
+		  <li class="tabs-title"><a href="#panel6">Sponsor Fees</a></li>
+		  <li class="tabs-title"><a href="#panel7">Registration</a></li>
+		</ul>
+		<div class="tabs-content" data-tabs-content="event-tabs">
+			<div class="tabs-panel is-active" id="panel1">
+				<ul>
+					<li>Band(s) comprised of mostly sober people. </li>
+					<li>Kid area with bounce house, face painting and prizes (coordinated by TSC)</li>
+					<li>BBQ served by sober living crew</li>
+					<li>Speaker(s)</li>
+					<li>Adult games </li>
+					<li>Race</li>
+					<li>Climbing wall</li>
+				</ul>
+			</div>
+			<div class="tabs-panel" id="panel2">
+				<p>Springs Recovery Connection, Phoenix MultiSport and the TSC Care Co-op are partnering together hold The Springs Recovery Rally and 5K Run/3K Walk on Saturday, September 10th from 9:00 AM to 2:00 PM at America the Beautiful Park, in downtown Colorado Springs.</p>
+
+				<p>This event will celebrate our successes as a recovery community, put a face and voice to recovery from substance abuse disorder, and reduce the stigma that so often surrounds the disease of addiction. We believe Colorado Springs is a great place to recover from substance use disorders and find freedom from addiction!</p>
+
+				<p>Join us at 9:00 AM for our 5K Run/3K Walk for Recovery, sponsored by Phoenix MultiSport. Over 200 runners are expected to participate. At 10:00 the rally begins as we celebrate National Recovery Month together. We will have live music, speakers, games and contests, a “kids zone” full of activities and prizes, great food and much more!  Representatives from healthcare organizations, in-patient and out-patient recovery centers, 12-step groups, non-profit organizations, and others committed to proving help to people seeking recovery and their families, will be onsite to share resources and hope. </p>
+
+				<p>Event sponsorship opportunities, race registration and additional information can be found at: <a href="http://springsrecoveryrally.org">springsrecoveryrally.org</a>. You may also call Cathy Plush for more info at: <a href="tel:7193673292">719-367-3292</a> or e-mail us at: <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a>.</p>
+
+				<p><strong><a href="http://www.springsrecoveryconnection.org/" target="new">Springs Recovery Connection</a></strong> <blockquote>Is a non-profit organization serving the community of Colorado Springs and surrounding areas. We offer a wide variety of services including information and referral, substance abuse awareness education, Recovery Coaching, sober activities and support group meetings. <a href="http://www.springsrecoveryconnection.org/" target="new">www.springsrecoveryconnection.org.</a></p></blockquote>
+
+				<p><strong><a href="http://www.phoenixmultisport.org/" target="new">Phoenix MultiSport</a></strong> <blockquote>Fosters a supportive, physically active community for individuals who are recovering from a substance use disorder and those who choose to live sober. Through pursuits such as climbing, hiking, running, strength training, yoga, road/mountain biking, socials and other activities, we seek to help our members develop and maintain the emotional strength they need to stay sober. <a href="http://www.phoenixmultisport.org" target="new">www.phoenixmultisport.org.</a></p></blockquote>
+
+				<p><strong><a href="http://www.tsccareco-op.org/" target="new">TSC Care Co-op</a></strong> <blockquote>Is a partnership of organizations in who provide accessible mental health and recovery care for people in Colorado Springs. As a group we provide donation-based individual, couples, youth, marriage and family counseling; secular and faith-based recovery resources groups and resources; and, are training the next generation of Licensed Professional Counselors and Certified/Licensed Addictions counselors in the State of Colorado. The TSC Care Co-op is a community-based resources, founded by The Sanctuary Church, a “recovery” church on the West Side. <a href="http://www.tsccareco-op.org/" target="new">www.tsccareco-op.org.</a></p></blockquote>
+			</div>
+			<div class="tabs-panel" id="panel4">
+				<p>Suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+			</div>
+			<div class="tabs-panel" id="panel5">
+				<div class="row">
+					<div class="sponsor">
+						<h3>Premier</h3>
+						<img src="http://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,h_100/v1464562416/peaks/Logos_Side_By_Side_Large-1.png">
+					</div>
+					<hr>
+				</div>
+				<div class="row">
+					<div class="sponsor">
+						<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+					</div>
+					<hr>
+				</div>
+				<div class="row">
+					<div class="sponsor">
+						<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+					</div>
+					<hr>
+				</div>
+				<div class="row">
+					<div class="sponsor">
+						<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+					</div>
+					<hr>
+				</div>
+			</div>
+			<div class="tabs-panel" id="panel6">
+			<h4>To begin the registration process please select one of the following sponsor fees.</h4>
+				<div class="row">
+					<div class="medium-8 columns">
+						<p><strong>Premier Sponsorship</strong> at $2,500<br />
+						Item included with sponsorship..</p>
+						<p><strong>Gold Sponsorship</strong> at $1,500/ea</p>
+						<p><strong>Silver Sponsorship</strong> at $500/ea</p>
+						<p><strong>Bronze Sponsors</strong> at $250/ea (will cap if we get too many)</p>
+					</div>
+					<div class="medium-4 columns">
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="JPHMT5NP7LXBU">
+						<table>
+						<tr><td><input type="hidden" name="on0" value="Sponsorship Level">Sponsorship Level</td></tr><tr><td><select name="os0">
+							<option value="Premier Sponsorship">Premier Sponsorship $2,500.00 USD</option>
+							<option value="Gold Sponsorship">Gold Sponsorship $1,500.00 USD</option>
+							<option value="Silver Sponsorship">Silver Sponsorship $500.00 USD</option>
+							<option value="Bronze Sponsorship">Bronze Sponsorship $250.00 USD</option>
+						</select> </td></tr>
+						</table>
+						<input type="hidden" name="currency_code" value="USD">
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+						</form>
+					</div>
+				</div>
+			</div>
+			<div class="tabs-panel" id="panel7">
+				<div class="row">
+					<h4>Register for 5k Run at $35 per person</h4><br />
+					<a href="http://www.active.com">
+						<button class="button" type="button">5k Run Registration</button>
+					</a>
+				</div>
+				<div class="row">
+					<h4>Volunteer Registration</h4><br />
+					<a href="#">
+						<button class="button" type="button">Volunteer Register</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Small Screens Only -->
+<div class="small-tabs">
+	<div class="row collapse show-for-small-only">
+		<div class="medium-3 columns">
+			<ul class="tabs vertical" id="example-vert-tabs" data-tabs>
+				<li class="tabs-title is-active"><a href="#panel1a" aria-selected="true">Event Overview</a></li>
+				<li class="tabs-title"><a href="#panel2a">Program</a></li>
+				<li class="tabs-title"><a href="#panel4a">Exhibit</a></li>
+				<li class="tabs-title"><a href="#panel5a">Sponsor</a></li>
+				<li class="tabs-title"><a href="#panel6a">Sponsor Fees</a></li>
+				<li class="tabs-title"><a href="#panel7a">Registration</a></li>
+			</ul>
+		</div>
+		<div class="medium-9 columns">
+			<div class="tabs-content vertical" data-tabs-content="example-vert-tabs">
+				<div class="tabs-panel is-active" id="panel1a">
+					<ul>
+						<li>Band(s) comprised of mostly sober people. </li>
+						<li>Kid area with bounce house, face painting and prizes (coordinated by TSC)</li>
+						<li>BBQ served by sober living crew</li>
+						<li>Speaker(s)</li>
+						<li>Adult games </li>
+						<li>Race</li>
+						<li>Climbing wall</li>
+					</ul>
+				</div>
+				<div class="tabs-panel" id="panel2a">
+					<p>Springs Recovery Connection, Phoenix MultiSport and the TSC Care Co-op are partnering together hold The Springs Recovery Rally and 5K Run/3K Walk on Saturday, September 10th from 9:00 AM to 2:00 PM at America the Beautiful Park, in downtown Colorado Springs.</p>
+
+					<p>This event will celebrate our successes as a recovery community, put a face and voice to recovery from substance abuse disorder, and reduce the stigma that so often surrounds the disease of addiction. We believe Colorado Springs is a great place to recover from substance use disorders and find freedom from addiction!</p>
+
+					<p>Join us at 9:00 AM for our 5K Run/3K Walk for Recovery, sponsored by Phoenix MultiSport. Over 200 runners are expected to participate. At 10:00 the rally begins as we celebrate National Recovery Month together. We will have live music, speakers, games and contests, a “kids zone” full of activities and prizes, great food and much more!  Representatives from healthcare organizations, in-patient and out-patient recovery centers, 12-step groups, non-profit organizations, and others committed to proving help to people seeking recovery and their families, will be onsite to share resources and hope.</p>
+
+					<p>Event sponsorship opportunities, race registration and additional information can be found at: <a href="http://springsrecoveryrally.org">springsrecoveryrally.org</a>. You may also call Cathy Plush for more info at: <a href="tel:7193673292">719-367-3292</a> or e-mail us at: <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a>.</p>
+
+					<p><strong><a href="http://www.springsrecoveryconnection.org/" target="new">Springs Recovery Connection</a></strong> <blockquote>Is a non-profit organization serving the community of Colorado Springs and surrounding areas. We offer a wide variety of services including information and referral, substance abuse awareness education, Recovery Coaching, sober activities and support group meetings. <a href="http://www.springsrecoveryconnection.org/" target="new">www.springsrecoveryconnection.org.</a></p></blockquote>
+
+					<p><strong><a href="http://www.phoenixmultisport.org/" target="new">Phoenix MultiSport</a></strong> <blockquote>Fosters a supportive, physically active community for individuals who are recovering from a substance use disorder and those who choose to live sober. Through pursuits such as climbing, hiking, running, strength training, yoga, road/mountain biking, socials and other activities, we seek to help our members develop and maintain the emotional strength they need to stay sober. <a href="http://www.phoenixmultisport.org" target="new">www.phoenixmultisport.org.</a></p></blockquote>
+
+					<p><strong><a href="http://www.tsccareco-op.org/" target="new">TSC Care Co-op</a></strong> <blockquote>Is a partnership of organizations in who provide accessible mental health and recovery care for people in Colorado Springs. As a group we provide donation-based individual, couples, youth, marriage and family counseling; secular and faith-based recovery resources groups and resources; and, are training the next generation of Licensed Professional Counselors and Certified/Licensed Addictions counselors in the State of Colorado. The TSC Care Co-op is a community-based resources, founded by The Sanctuary Church, a “recovery” church on the West Side. <a href="http://www.tsccareco-op.org/" target="new">www.tsccareco-op.org.</a></p></blockquote>
+				</div>
+				<div class="tabs-panel" id="panel4a">
+					<h4>Exhibit</h4>
+				</div>
+				<div class="tabs-panel" id="panel5a">
+					<div class="row">
+						<div class="sponsor">
+							<h3>Premier</h3>
+							<img src="http://res.cloudinary.com/dbc2wlvk8/image/upload/c_scale,h_100/v1464562416/peaks/Logos_Side_By_Side_Large-1.png">
+						</div>
+						<hr>
+					</div>
+					<div class="row">
+						<div class="sponsor">
+							<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+						</div>
+						<hr>
+					</div>
+					<div class="row">
+						<div class="sponsor">
+							<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+						</div>
+						<hr>
+					</div>
+					<div class="row">
+						<div class="sponsor">
+							<p>Please Contact us at <a href="mailto:info@springsrecoveryconnection.org">info@springsrecoveryconnection.org</a> to be a sponsor!</p>
+						</div>
+						<hr>
+					</div>
+				</div>
+				<div class="tabs-panel" id="panel6a">
+					<h4>To begin the registration process please select one of the following sponsor fees.</h4>
+					<div class="row">
+						<div class="small-12 columns">
+							<p><strong>Premier Sponsorship</strong> at $2,500<br />
+							Item included with sponsorship..</p>
+							<p><strong>Gold Sponsorship</strong> at $1,500/ea</p>
+							<p><strong>Silver Sponsorship</strong> at $500/ea</p>
+							<p><strong>Bronze Sponsors</strong> at $250/ea (will cap if we get too many)</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="small-12 columns">
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="JPHMT5NP7LXBU">
+							<table>
+							<tr><td><input type="hidden" name="on0" value="Sponsorship Level">Sponsorship Level</td></tr><tr><td><select name="os0">
+								<option value="Premier Sponsorship">Premier Sponsorship $2,500.00 USD</option>
+								<option value="Gold Sponsorship">Gold Sponsorship $1,500.00 USD</option>
+								<option value="Silver Sponsorship">Silver Sponsorship $500.00 USD</option>
+								<option value="Bronze Sponsorship">Bronze Sponsorship $250.00 USD</option>
+							</select> </td></tr>
+							</table>
+							<input type="hidden" name="currency_code" value="USD">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+							</form>
+						</div>
+					</div>
+				</div>
+				<div class="tabs-panel" id="panel7a">
+					<div class="row">
+						<h4>Register for 5k Run at $35 per person</h4><br />
+						<a href="http://www.active.com">
+							<button class="button" type="button">5k Run Registration</button>
+						</a>
+					</div>
+					<div class="row">
+						<h4>Volunteer Registration</h4><br />
+						<a href="#">
+							<button class="button" type="button">Volunteer Register</button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<!-- Scripts -->
+<script src="https://cdn.jsdelivr.net/jquery/2.2.3/jquery.min.js"></script>
+<script src="//cdn.jsdelivr.net/foundation/6.2.1/foundation.min.js"></script>
+
+
+ <script>
+  $(document).foundation();
+</script>
+<!-- Scripts -->
+</body>
+</html>
